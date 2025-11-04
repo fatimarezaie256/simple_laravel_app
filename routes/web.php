f76c210f1239ac6c2e6002d1cdb5a27ebb6ee3be
fatimarezaie256 <fatimarezaie112@gmail.com>
2025-11-04 18:29:04 +0430

    the about view created

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/', function () {
+    return "Hi this is the home page";
+});
+Route::get('/about', function(){
+    $myname = "sara";
+    $myage = 23;
+    return view('about')->with('name',$myname,)->with('age',$myage);
+});
